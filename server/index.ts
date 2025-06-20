297c6f2e402875e307f0c5c8b44e71c43b13573d
mvbican <42104816-mvbican@users.noreply.replit.com>
2025-06-20 02:28:12 +0000

    Restored to '778461b1fe9d8ff3293c94c4f679c469743bca42'
    Replit-Restored-To: 778461b1fe9d8ff3293c94c4f679c469743bca42

--- server/index.ts
+++ server/index.ts
@@ -53,8 +53,7 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    const { serveStaticWithSSR } = await import("./ssr-production.js");
-    serveStaticWithSSR(app);
+    serveStatic(app);
   }
 
   // ALWAYS serve the app on port 5000
